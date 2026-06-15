d65947c669e04445fc967ba2f3c5ff7a234e12ab
yewinthlaing04 <yewinthlaing23@gmail.com>
2026-06-15 13:13:45 +0200

    project start up

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Home page",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
